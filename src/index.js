4a17ab668e65399c0dc2208d2dc9644e27af095f
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-07-18 17:22:50 -0300

    type: modify file file: ./src/index.js description: hola mundo expres

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("hola munado!");
+const PORT = process.env.port || 3000;
+const https = require('https');
+const express = require('express');
+const app = express();
+
+
+app.get('/',(req,res) =>{
+    res.contentType('text/plain');
+    res.send('hola mundo!');
+})
+
+https.createServer(app)
+.listen(PORT,() => {
+    console.log(`online! \nlistemport: ${PORT}`);
+});
+
