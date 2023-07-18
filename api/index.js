eb9f85a61fc127212e483526ed31003d96640108
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-07-18 19:53:22 -0300

    type: modify file: ./api/index.js dexcription: add temporary db conection test

--- api/index.js
+++ api/index.js
@@ -9,6 +9,11 @@ const app = express();
 app.use('/api',express.json());
 app.use('/api',cors());
 
+mongoose.connect(process.env.MONGODB_URL,{
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+},() => console.log('mongoose conected!'));
+
 app.get('/api',(req,res) =>{
     res.contentType('text/plain');
     res.send('hola mundo!');
