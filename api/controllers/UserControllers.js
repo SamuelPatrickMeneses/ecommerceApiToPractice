a858ee131ffbce139b126430f1929842a94dd6a0
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-07-28 17:22:52 -0300

    type: modify file file: ./api/controllers/UserControllers.js description: implements user controler.

--- api/controllers/UserControllers.js
+++ api/controllers/UserControllers.js
@@ -1,13 +1,28 @@
+const userServise = require('../services/UserServices.js');
+const errHandler = require('../helpers/defaltErrorHandler.js');
 function getUsers(req, res){
-
+    userServise.getUsers()
+    .then((data) => {
+        res.status(200)
+        .json(data);
+    }).catch(errHandler(res));
 }
 
 function postUser(req, res){
-
+    userServise.crateUser(req.body)
+    .then((data) => {
+        res.status(201)
+        .json(data);
+    }).catch(errHandler(res));
 }
 
 function getUserById(req, res){
-
-}
+    const userId = req.params['user_id'];
+    userServise.getUsersById(userId)
+    .then((data) => {
+        res.status(200)
+        .json(data);
+    }).catch(errHandler(res));
+};
 
 module.exports = {getUsers,getUserById,postUser};
