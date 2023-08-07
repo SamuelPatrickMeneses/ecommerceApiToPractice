8e571141a74c078fabd559328f8014e4b0961591
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-08-07 18:22:45 -0300

    type: mofdify file file: ./app/services/UserServices.js description: add dtos, userPass encript and user by name query.

--- app/services/UserServices.js
+++ app/services/UserServices.js
@@ -1,5 +1,7 @@
 const userRepository = require('../models/User.js');
 const {isConectade} = require('../config/DBConection.js');
+const {bcryptSalt} = require('../config/securityConfig.js');
+const bcrypt = require('bcryptjs');
 function formatError(name, errors){
     return {
         "name": isConectade ? name : 'DatabaseError',
@@ -11,8 +13,8 @@ module.exports = {
     userToDTO({_id, address, payment, userName}){
         return {id: _id, address, payment, name: userName};
     },
-    DTOToUser({id, address, payment, name}){
-        return {_id: id, address, payment, userName: name};
+    DTOToUser({id, address, payment, name, pass}){
+        return {_id: id, address, payment, userName: name, userPass: pass};
     },
     async  getUsers(){
         try{
@@ -25,6 +27,7 @@ module.exports = {
     async crateUser(dto){
         try{
             let user = this.DTOToUser(dto);
+            user.userPass = await bcrypt.hash(user.userPass, bcryptSalt);
             user = await userRepository.create(user);
             return this.userToDTO(user);
         }catch(err){
@@ -34,6 +37,14 @@ module.exports = {
     async  getUsersById(id){
         try{
             const user = await userRepository.findOne({_id: id});
+            return this.userToDTO(user);
+        }catch({name, errors}){
+            throw formatError(name, error);
+        }
+    },
+    async  getUsersByName(name){
+        try{
+            const user = await userRepository.findOne({userName: name});
             return this.userToDTO(user);
         }catch({name, errors}){
             throw formatError(name, error);
