11797a54a46224b194ac8e30ae0dd5c1c71e61df
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-08-07 18:57:39 -0300

    type: modify file file app/config/responseErrosFereferece.js description: define new errors

--- app/config/responseErrosReference.js
+++ app/config/responseErrosReference.js
@@ -2,5 +2,7 @@ module.exports = {
     ValidationError: {status: 400},
     MongooseError: {status: 500},
     DatabaseError: {status: 500},
-    ReferenceError: {status: 404}
+    ReferenceError: {status: 404},
+    NotFoundError: {status: 404},
+    AuthenticationError: {status: 401}
 };
